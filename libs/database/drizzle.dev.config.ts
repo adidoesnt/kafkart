4dbf1eda6b613a98d9b06fb30200ccd3dfbcd2ba
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-19 18:00:18 +0800

    update migration dirs

--- libs/database/drizzle.dev.config.ts
+++ libs/database/drizzle.dev.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { DB_URL } from "@constants";
 
 export default defineConfig({
-	out: "./drizzle/dev",
+	out: "./drizzle/migrations/dev",
 	schema: "./lib/database/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
--- libs/database/drizzle.prod.config.ts
+++ libs/database/drizzle.prod.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { DB_URL } from "@constants";
 
 export default defineConfig({
-	out: "./drizzle/prod",
+	out: "./drizzle/migrations/prod",
 	schema: "./lib/database/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
