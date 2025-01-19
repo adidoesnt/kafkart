fc4b37d31b070141d0668f6ea253bfe3d5d682f8
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-19 16:27:17 +0800

    add separate drizzle configs for different envs

---
+++ libs/database/drizzle.dev.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import { DB_URL } from "@constants";
+
+export default defineConfig({
+	out: "./drizzle/dev",
+	schema: "./lib/database/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: DB_URL,
+	},
+});
--- libs/database/drizzle.prod.config.ts
+++ libs/database/drizzle.prod.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { DB_URL } from "@constants";
 
 export default defineConfig({
-	out: "./drizzle",
+	out: "./drizzle/prod",
 	schema: "./lib/database/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
