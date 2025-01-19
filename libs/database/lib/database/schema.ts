4571ab8c493fcf6ef029597c5c56da1d8a959eed
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-19 16:44:38 +0800

    update user schema

--- libs/database/lib/database/schema.ts
+++ libs/database/lib/database/schema.ts
@@ -1,8 +1,9 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, boolean } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	username: varchar({ length: 255 }).notNull().unique(),
 	passwordHash: varchar({ length: 255 }).notNull(),
 	email: varchar({ length: 255 }).notNull().unique(),
+	isAdmin: boolean().notNull().default(false),
 });
