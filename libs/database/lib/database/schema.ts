dbeab133003fd7a0b3052e9d27a3dea462fb216e
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-01-19 16:51:03 +0800

    add products and orders tables

--- libs/database/lib/database/schema.ts
+++ libs/database/lib/database/schema.ts
@@ -7,3 +7,20 @@ export const usersTable = pgTable("users", {
 	email: varchar({ length: 255 }).notNull().unique(),
 	isAdmin: boolean().notNull().default(false),
 });
+
+export const productsTable = pgTable("products", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	name: varchar({ length: 255 }).notNull(),
+	description: varchar({ length: 255 }).notNull(),
+	price: integer().notNull(),
+	imageUrl: varchar({ length: 255 }).notNull(),
+    stock: integer().notNull().default(0),
+});
+
+export const ordersTable = pgTable("orders", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	userId: integer().notNull(),
+	productId: integer().notNull(),
+	quantity: integer().notNull(),
+	totalPrice: integer().notNull(),
+});
