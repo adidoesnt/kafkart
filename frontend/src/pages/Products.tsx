cdd6b031c030dcaeb8446fce7686ca672ae12cb3
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-11 12:56:36 +0000

    add frontend solace client

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,5 +1,8 @@
 import { Layout } from "@/components/Layout";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/auth";
 import { productApiClient } from "@/utils/apiClient";
+import { publishProductView } from "@/utils/solace";
 import { getStockBadge } from "@/utils/stock";
 import { useCallback, useEffect, useState } from "react";
 
@@ -13,6 +16,7 @@ type Product = {
 };
 
 function Products() {
+	const { user } = useAuth();
 	const [products, setProducts] = useState<Product[]>([]);
 
 	const fetchProducts = useCallback(async () => {
@@ -35,26 +39,41 @@ function Products() {
 			<div className="flex flex-col w-full h-full justify-center items-center gap-8 py-8 overflow-hidden">
 				<h1 className="text-2xl font-bold">All Products</h1>
 				<div className="grid grid-flow-row grid-cols-3 gap-8 w-full h-full items-center justify-center overflow-auto">
-					{products.map((product) => (
-						<div
-							key={product.id}
-							className="flex flex-col justify-center items-center gap-2 bg-gray-600 rounded-md p-4 w-fit h-full text-center"
-						>
-							<h2 className="text-xl font-bold">
-								{product.name}
-							</h2>
-							<img
-								className="w-80 aspect-square h-80 rounded-md"
-								src={product.imageUrl}
-								alt={product.name}
-							/>
-							<p className="text-sm">{product.description}</p>
-							<p className="text-sm font-semibold">Price: ${product.price}</p>
-							<p className="text-sm">
-								{getStockBadge(product.stock)}
-							</p>
-						</div>
-					))}
+					{products.map((product) => {
+						const onClick = async () => {
+							await publishProductView(user!.id, product.id);
+						};
+
+						return (
+							<div
+								key={product.id}
+								className="flex flex-col justify-center items-center gap-2 bg-gray-600 rounded-md p-4 w-fit h-full text-center"
+							>
+								<h2 className="text-xl font-bold">
+									{product.name}
+								</h2>
+								<img
+									className="w-80 aspect-square h-80 rounded-md"
+									src={product.imageUrl}
+									alt={product.name}
+								/>
+								<p className="text-sm">{product.description}</p>
+								<p className="text-sm font-semibold">
+									Price: ${product.price}
+								</p>
+								<p className="text-sm">
+									{getStockBadge(product.stock)}
+								</p>
+								<br />
+								<Button
+									className="bg-gray-200 hover:bg-gray-400 text-gray-900"
+									onClick={onClick}
+								>
+									View Product
+								</Button>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</Layout>
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,8 @@
 export const {
 	USER_API_BASE_URL = "http://localhost:3000/users",
 	PRODUCT_API_BASE_URL = "http://localhost:3000/products",
+	SOLACE_HOST = "ws://localhost:8008",
+	SOLACE_VPN_NAME = "default",
+	SOLACE_USERNAME = "admin",
+	SOLACE_PASSWORD = "password",
 } = import.meta.env;
